044b34c7a155bcca2210a91b126f3f9fc5c44656
prenl <elnur.kazakhstan@gmail.com>
2023-06-06 23:56:03 +0600

    Update helpers.py

--- core/utils/helpers.py
+++ core/utils/helpers.py
@@ -1,6 +1,10 @@
-from aiogram import types
+from aiogram import types, Router
+from aiogram.filters import Text
 
-@dp.message_handler(Text(equals="How to find token?", ignore_case=True))
+router = Router()
+
+
+@router.message(Text("How to find token?", ignore_case=True))
 async def find_moodle_token(message: types.Message):
     await message.answer("To find your token you should go to your moodle profile, "
                          "click on the settings at the top right corner, "
