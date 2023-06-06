891e9d2972162017c3e194b36f59b13319e8cbc3
prenl <elnur.kazakhstan@gmail.com>
2023-06-06 23:56:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from aiogram import *
+import logging
 import asyncio
 import dotenv
 import os
-from core.handlers import questions, different_types
+from core.utils import helpers
+from core.handlers.user_handlers import user_handlers
+
 
 async def main():
     dotenv.load_dotenv()
@@ -14,9 +17,13 @@ async def main():
 
     bot = Bot(token, parse_mode="HTML")
     print("Bot started!")
-    dp.include_routers(questions.router, different_types.router)
+    dp.include_routers(user_handlers.router, helpers.router)
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
     asyncio.run(main())
