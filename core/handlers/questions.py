460ab6464c4577a16a00ce1178d9e24fd7b28eac
prenl <elnur.kazakhstan@gmail.com>
2023-06-06 23:55:57 +0600

    Update questions.py

--- core/handlers/questions.py
+++ core/handlers/questions.py
@@ -1,11 +1,12 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.filters.text import Text
+from aiogram.filters import Text
 from aiogram.types import Message, ReplyKeyboardRemove
 
 from core.keyboards.for_questions import get_yes_no_kb
 
 router = Router()  # [1]
+
 
 @router.message(Command("start"))  # [2]
 async def cmd_start(message: Message):
@@ -14,12 +15,14 @@ async def cmd_start(message: Message):
         reply_markup=get_yes_no_kb()
     )
 
+
 @router.message(Text(text="да", ignore_case=True))
 async def answer_yes(message: Message):
     await message.answer(
         "Сочувствую",
         reply_markup=ReplyKeyboardRemove()
     )
+
 
 @router.message(Text(text="нет", ignore_case=True))
 async def answer_no(message: Message):
