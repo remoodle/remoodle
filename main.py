d9380cde7c8c34fa23911a5c6dd463415db8c8e1
prenl <elnur.kazakhstan@gmail.com>
2023-06-07 19:51:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ async def main():
     dotenv.load_dotenv()
     token = os.getenv("TOKEN")
 
-    router = Router()
     dp = Dispatcher()
-    dp.include_router(router)
+
 
     bot = Bot(token, parse_mode="HTML")
     print("Bot started!")
