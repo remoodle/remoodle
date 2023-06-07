e7ca3abb31da036958dbf32b3849f1012f246183
prenl <elnur.kazakhstan@gmail.com>
2023-06-07 21:37:23 +0600

    Update user_handlers.py

--- core/handlers/user_handlers/user_handlers.py
+++ core/handlers/user_handlers/user_handlers.py
@@ -1,18 +1,14 @@
-from aiogram import Router, types, Bot
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
-from aiogram.utils.keyboard import *
-from core.db.database import Database
-from core.api.api import Api
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from core.keyboards.user_menu import *
 from core.utils.statesform import StepsForm
 from core.utils.helpers import *
-from main import db, api
+from main import db
 
 
 router = Router()
-
 
 
 @router.message(Command("start"))
@@ -26,9 +22,9 @@ async def start(message: types.Message, state: FSMContext):
         await message.answer("Enter your token: ", reply_markup=k_button.as_markup(resize_keyboard=True))
         await state.set_state(StepsForm.GET_MOODLE_TOKEN)
     else:
-        user = api.get_user_info(db.get_token(user_id))
+        full_name = db.get_full_name(user_id)
         await message.answer(
-            text=f"Hello, *{user['full_name']}*\nSelect an option: ",
+            text=f"Hello, *{full_name}*\nSelect an option: ",
             parse_mode='Markdown',
             reply_markup=main_menu()
         )
@@ -48,55 +44,54 @@ async def save_moodle_token(message: types.Message, state: FSMContext):
             db.insert_token(user_id=message.from_user.id, token=message.text)
         await define_token(message)
         await state.clear()
-        await get_moodle_token(message, state)
+        await start(message, state)
 
 
 @router.callback_query(Text(text="deadlines"))
 async def deadlines_handler(call: CallbackQuery):
-    await call.message.edit_text(text="Your deadlines:")
-    await call.message.edit_reply_markup(reply_markup=back())
+    await call.message.edit_text(text="Your deadlines:",
+                                 reply_markup=back())
     await call.answer()
 
 
 @router.callback_query(Text(text="grades"))
 async def grades_handler(call: CallbackQuery):
-    await call.message.edit_text("Choose course group:")
-    await call.message.edit_reply_markup(reply_markup=grades_menu())
+    await call.message.edit_text("Choose course group:",
+                                 reply_markup=grades_menu())
     await call.answer()
 
 
 @router.callback_query(Text(text="grades_menu_all_courses"))
 async def all_grades_handler(call: CallbackQuery):
-    await call.message.edit_text("Choose course:")
-    await call.message.edit_reply_markup(reply_markup=grades_menu_all_courses(call.message.chat.id))
+    await call.message.edit_text("Choose course:",
+                                 reply_markup=grades_menu_all_courses(call.message.chat.id))
     await call.answer()
 
 
 @router.callback_query(Text(text="grades_menu_current_courses"))
 async def current_grades_handler(call: CallbackQuery):
-    await call.message.edit_text("Choose course:")
-    await call.message.edit_reply_markup(reply_markup=grades_menu_current_courses(call.message.chat.id))
+    await call.message.edit_text("Choose course:",
+                                 reply_markup=grades_menu_current_courses(call.message.chat.id))
     await call.answer()
 
 
 @router.callback_query(Text(contains="course_"))
 async def course_grades_handler(call: CallbackQuery):
-    await call.message.edit_text(str(create_grades_string(call.data.split("_")[1], call.message.chat.id)))
-    await call.message.edit_reply_markup(reply_markup=back())
+    await call.message.edit_text(str(create_grades_string(call.data.split("_")[1], call.message.chat.id)),
+                                 reply_markup=back())
+    await call.answer()
 
 
 @router.callback_query(Text(text="back_to_menu"))
 async def back_to_menu_handler(call: CallbackQuery):
-    user_id = call.message.chat.id
-    user = api.get_user_info(db.get_token(user_id))
-    await call.message.edit_text(text=f"Hello, *{user['full_name']}*\nSelect an option: ",
-            parse_mode='Markdown')
-    await call.message.edit_reply_markup(reply_markup=main_menu())
+    full_name = db.get_full_name(call.message.chat.id)
+    await call.message.edit_text(text=f"Hello, *{full_name}*\nSelect an option: ",
+                                 reply_markup=main_menu(),
+                                 parse_mode="Markdown")
     await call.answer()
 
 
 @router.callback_query(Text(text="back_to_grades"))
-async def test1(call: CallbackQuery):
+async def back_to_grades_handler(call: CallbackQuery):
     await call.message.edit_reply_markup(reply_markup=grades_menu())
     await call.answer()
-
