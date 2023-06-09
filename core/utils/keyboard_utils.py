df4684d2a66bb2839a32b35f0e7c2a065bae7dbd
prenl <elnur.kazakhstan@gmail.com>
2023-06-09 15:54:56 +0600

    delete unused files

--- core/utils/keyboard_utils.py
+++ core/utils/keyboard_utils.py
@@ -2,11 +2,13 @@ from core.db.database import Database
 
 db = Database()
 
+
 def get_grade_notifications_state(user_id):
     if db.get_grade_notification(user_id) == 1:
         return "🟢"
     else:
         return "🔴"
+
 
 def get_deadline_notifications_state(user_id):
     deadline_state = db.get_deadline_notification(user_id)
@@ -15,6 +17,7 @@ def get_deadline_notifications_state(user_id):
     else:
         deadline_state='🔴'
     return deadline_state
+
 
 def get_user_token(user_id):
     return db.get_token(user_id)
