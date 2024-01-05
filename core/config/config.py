aadd6392bbd7ce1fc67e4bc4ba48b531cc9065be
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-06 02:55:17 +0600

    Mongodb

---
+++ core/config/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
+
+find_dotenv()
+load_dotenv()
+
+DB_NAME = getenv("DB_NAME")
+DB_USER = getenv("DB_USER")
+DB_PASS = getenv("DB_PASS")
+BOT_TOKEN = getenv("BOT_TOKEN")
+DB_HOST = getenv("DB_HOST")
+
---
+++ core/db/database.py
@@ -0,0 +1,14 @@
+from core.config.config import DB_NAME, DB_USER, DB_PASS, DB_HOST
+from mongoengine import *
+
+connect(host=f'mongodb+srv://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}')
+
+class User(Document):
+    id = IntField(required=True)
+    username = StringField(required=True)
+    hashed_token = StringField(required=True)
+    full_name = StringField(required=True)
+    barcode = IntField(required=True)
+    grades_notification = BooleanField(default=True)
+    deadlines_notification = IntField(default=0)
+
