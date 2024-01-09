998d9a4d4f72f9767b9eb49d670f72862f5a1f73
DeKarLo <karen.ananyan2017@mail.ru>
2024-01-09 17:03:31 +0600

    db utils

--- core/utils/db_utils.py
+++ core/utils/db_utils.py
@@ -4,19 +4,31 @@ from core.encoder.chiper import Enigma
 from core.moodle.moodleservice import Service
 
 
-def create_user(
-        telegram_id: int,
-        token: str
-):
-    user = await Service.get_user_info(token)
-    new_user = User(
-        telegram_id=telegram_id,
-        username=get_telegram_username(telegram_id),
-        hashed_token=Enigma.encrypt(token),
-        full_name=user.full_name,
-        barcode=user.barcode,
-        grades_notification=True,
-        deadlines_notification=1,
-        is_admin=False
-    )
-    new_user.save()
+async def create_user(telegram_id: int, token: str):
+    try:
+        user_info = await Service.get_user_info(token)
+        new_user = User(
+            telegram_id=telegram_id,
+            username=get_telegram_username(telegram_id),
+            hashed_token=Enigma.encrypt(token),
+            full_name=user_info.full_name,
+            barcode=user_info.barcode,
+            grades_notification=True,
+            deadlines_notification=1,
+            is_admin=False
+        )
+        new_user.save()
+    except Exception as e:
+        print(f"Error creating user: {str(e)}")
+
+def delete_user(user_id):
+    try:
+        user = User.objects(telegram_id=user_id).first()
+        if user:
+            user.delete()
+        else:
+            print(f"User with telegram_id {user_id} not found.")
+    except Exception as e:
+        print(f"Error deleting user: {str(e)}")
+
+
