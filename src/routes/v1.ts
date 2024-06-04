22d2a6aca5bae63774329f0f000d4879635383c5
Alex Sehl <widesehl@gmail.com>
2024-06-05 01:31:12 +0500

    go => node.js

---
+++ src/routes/v1.ts
@@ -0,0 +1,74 @@
+import { Hono } from "hono";
+import { User } from "../database";
+import { genToken } from "../utils";
+
+const api = new Hono();
+
+api.post("/users", async (c) => {
+  const { name, email, password } = await c.req.json();
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    c.status(400);
+    return c.json({ message: "User already exists" });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (!user) {
+    c.status(400);
+    return c.json({ message: "Invalid user data" });
+  }
+
+  const token = await genToken(user._id.toString());
+
+  return c.json({
+    success: true,
+    data: {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    token,
+    message: "User created successfully",
+  });
+});
+
+api.post("/users/login", async (c) => {
+  const { email, password } = await c.req.json();
+
+  if (!email || !password) {
+    c.status(400);
+    return c.json({ message: "Please provide an email and password" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    c.status(401);
+    return c.json({ message: "No user found with this email" });
+  }
+
+  if (!(await user.verifyPassword(password))) {
+    c.status(401);
+    throw new Error("Invalid credentials");
+  } else {
+    const token = await genToken(user._id.toString());
+
+    return c.json({
+      success: true,
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+      message: "User logged in successfully",
+    });
+  }
+});
+
+export default api;
