d3af7acea45c3f0c01bb4a5af99858dc2d38e868
Alex <widesehl@gmail.com>
2024-08-19 00:28:43 +0300

    Update index.ts

--- packages/hc-wrapper/src/index.ts
+++ packages/hc-wrapper/src/index.ts
@@ -1,6 +1,6 @@
 import { hc } from "hono/client";
-import type { Hono } from "hono";
 import type { ClientResponse } from "hono/client";
+import type { Hono } from "hono";
 import type { StatusCode } from "hono/utils/http-status";
 import type { APIErrorResponse, APIError } from "@remoodle/types";
 
