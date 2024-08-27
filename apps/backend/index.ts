c3ce9a235761c523bc07bec77b31f6ab941dfcc9
Alex Sehl <widesehl@gmail.com>
2024-08-27 16:04:05 +0500

    fix: bundle shit properply

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,3 +1,2 @@
 export type { AppType } from "./src/services/server";
 export type { IUser } from "./src/library/db/mongo/models/User";
-// 
--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig((options) => ({
+export default defineConfig(() => ({
   entry: ["src/app.ts"],
   clean: true,
   format: ["cjs"],
   target: "node20",
+  noExternal: ["@remoodle/utils"],
 }));
