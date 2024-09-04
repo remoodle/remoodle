3d0ea474e4ff1809c0ecf102b92e2c4330ddc4c3
Yelnur <79540438+prenl@users.noreply.github.com>
2024-09-04 17:02:39 +0500

    fix: add moodleToken, remove type (#9)

--- src/Controllers/CourseContentController.php
+++ src/Controllers/CourseContentController.php
@@ -27,7 +27,7 @@ class CourseContentController extends BaseController
 
         $courses = $this->userMoodleRepository->getActiveCourses(
             moodleId: $user->moodle_id,
-            type: "all"
+            moodleToken: $user->moodle_token,
         );
 
         foreach($courses as $cours) { // - govno
