64f8fd42290137fc0dcacd218fd13e45ddc4a969
Alex Sehl <widesehl@gmail.com>
2024-09-07 01:15:13 +0500

    fix: add error handling

--- apps/telegram-bot/src/services/bot/index.ts
+++ apps/telegram-bot/src/services/bot/index.ts
@@ -1,4 +1,4 @@
-import { Bot, Context } from "grammy";
+import { Bot, Context, GrammyError, HttpError } from "grammy";
 import commandsHandler from "./handlers/commands";
 import callbacksHandler from "./handlers/callbacks";
 
@@ -7,6 +7,20 @@ export function createBot(token: string) {
 
   bot.use(commandsHandler);
   bot.use(callbacksHandler);
+
+  bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+      console.error("Could not contact Telegram:", e);
+    } else {
+      console.error("Unknown error:", e);
+    }
+  });
 
   return bot;
 }
