17c56bcc52d6ac55e49d741d1d3f6f0b7992e058
Alex Sehl <widesehl@gmail.com>
2024-09-07 01:59:29 +0500

    fix: wait a bit

--- apps/backend/src/services/notifier/index.ts
+++ apps/backend/src/services/notifier/index.ts
@@ -1,10 +1,21 @@
 import cron from "node-cron";
 import { db } from "../../library/db";
+import { env } from "../../config";
 import { fetchCourses, fetchDeadlines } from "./crawler";
 import { GradeChangeEventHandler } from "./grade-change-event";
 import { DeadlineReminderEventHandler } from "./deadline-reminder-event";
 
 export async function startNotifier() {
+  const grades = new GradeChangeEventHandler(db.messageStream);
+  const deadlines = new DeadlineReminderEventHandler(db.messageStream);
+
+  await Promise.all([grades.runJob(), deadlines.runJob()]);
+
+  if (env.isProduction) {
+    // wait for 30 seconds just in case
+    await new Promise((resolve) => setTimeout(resolve, 30_000));
+  }
+
   cron.schedule(
     "*/5 * * * *", // every 5 minutes
     () => fetchCourses(db.messageStream),
@@ -16,9 +27,4 @@ export async function startNotifier() {
     () => fetchDeadlines(db.messageStream),
     { runOnInit: true },
   );
-
-  const grades = new GradeChangeEventHandler(db.messageStream);
-  const deadlines = new DeadlineReminderEventHandler(db.messageStream);
-
-  await Promise.all([grades.runJob(), deadlines.runJob()]);
 }
