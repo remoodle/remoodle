0bb8545cbeda328a43c88df1d168c284f916acfa
prenl <79540438+prenl@users.noreply.github.com>
2024-09-24 12:34:30 +0500

    feat: long polling => webhook

--- apps/telegram-bot/src/app.ts
+++ apps/telegram-bot/src/app.ts
@@ -1,7 +1,11 @@
 import { config } from "./config";
 import { createBot } from "./bot";
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { webhookCallback } from "grammy";
 
 function main(): void {
+  const app = new Hono();
   const bot = createBot(config.bot.token);
 
   process.once("SIGINT", () => {
@@ -12,8 +16,14 @@ function main(): void {
     bot.stop();
   });
 
-  console.log("Bot is running");
+  if (!config.bot.isDev) {
+    app.use("/webhook", webhookCallback(bot, "hono"));
+    serve(app).listen({ port: config.server.port }, () => {
+      console.log(`Server is running on port ${config.server.port}`);
+    });
+  }
 
+  console.log("Bot is running");
   bot.start();
 }
 
