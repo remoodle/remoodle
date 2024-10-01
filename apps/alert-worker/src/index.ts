b73a6eca7bdd3ca063d01fa4282fc1c25c11639c
Alex Sehl <widesehl@gmail.com>
2024-10-01 20:29:13 +0500

    feat: add grafana webhook

--- apps/alert-worker/src/index.ts
+++ apps/alert-worker/src/index.ts
@@ -4,7 +4,7 @@ import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 import { Telegram } from "@remoodle/utils";
 
-const zTopicType = z.enum(["users", "users2", "dev", "errors"]);
+const zTopicType = z.enum(["users", "users2", "dev", "errors", "grafana"]);
 
 type TopicType = z.infer<typeof zTopicType>;
 
@@ -13,12 +13,10 @@ const TOPICS: Record<TopicType, number> = {
   users2: 329,
   dev: 327,
   errors: 309,
+  grafana: 3115,
 } as const;
 
 const app = new Hono<{
-  Variables: {
-    topicsMap: Record<string, number>;
-  };
   Bindings: {
     TELEGRAM_CHAT_ID: string;
     TELEGRAM_BOT_TOKEN: string;
@@ -56,6 +54,36 @@ const app = new Hono<{
 
       const resp = await telegram.notify(message, {
         topicId,
+        parseMode: "HTML",
+      });
+
+      if (resp.status !== 200) {
+        console.error(await resp.json());
+        return ctx.text(resp.statusText, 400);
+      }
+
+      return ctx.text("OK", 200);
+    },
+  )
+  .post(
+    "/webhook/grafana",
+    zValidator(
+      "json",
+      z.object({
+        title: z.string(),
+        message: z.string(),
+      }),
+    ),
+    async (ctx) => {
+      const { title, message } = ctx.req.valid("json");
+
+      const telegram = new Telegram(
+        ctx.env.TELEGRAM_BOT_TOKEN,
+        ctx.env.TELEGRAM_CHAT_ID,
+      );
+
+      const resp = await telegram.notify(`<b>${title}</b>\n${message}`, {
+        topicId: TOPICS["grafana"],
         parseMode: "HTML",
       });
 
