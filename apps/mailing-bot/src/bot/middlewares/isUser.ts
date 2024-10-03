c3ca044e9ba5c4ecd4a6262f9c4f3cff432cdc26
Karen Ananyan <karen.ananyan2017@mail.ru>
2024-10-04 01:00:27 +0500

    fix: refactor isUser middleware

--- apps/mailing-bot/src/bot/middlewares/isUser.ts
+++ apps/mailing-bot/src/bot/middlewares/isUser.ts
@@ -1,7 +1,7 @@
 import { ContextWithSession } from '..';
 
-export async function isAdmin(ctx: ContextWithSession, next: () => Promise<void>) {
-    if (ctx.session.role !== 'user') {
+export async function isUser(ctx: ContextWithSession, next: () => Promise<void>) {
+    if (ctx.session.role !== 'user' && ctx.session.role !== 'admin') {
         await ctx.reply("You dont have permission to use this command.");
         return; 
     }
