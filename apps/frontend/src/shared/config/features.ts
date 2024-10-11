4d61084c7bf3830c1c093fb15a2fd5e8a466654f
Alex Sehl <widesehl@gmail.com>
2024-10-11 20:45:32 +0500

    fix: revert shit

--- apps/frontend/src/shared/config/features.ts
+++ apps/frontend/src/shared/config/features.ts
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 const enableByDefault =
-  import.meta.env.MODE === "staging" || import.meta.env.DEV;
+  import.meta.env.MODE === "preview" || import.meta.env.DEV;
 
 export const features = reactive({
   enableTokenAuth: enableByDefault,
