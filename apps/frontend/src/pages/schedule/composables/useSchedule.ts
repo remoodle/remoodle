2dd56179ca9b825f44d837f06da30f1480c54719
prenl <79540438+prenl@users.noreply.github.com>
2025-01-07 01:07:56 +0500

    fix: time grid view

--- apps/frontend/src/pages/schedule/composables/useSchedule.ts
+++ apps/frontend/src/pages/schedule/composables/useSchedule.ts
@@ -62,7 +62,7 @@ export function useSchedule() {
     const groupSchedule = allSchedules.value[group];
 
     // Convert the schedule to CalendarEvent format (also for the previous and next week)
-    const resultSchedule: CalendarEvent[] = groupSchedule.flatMap((item) => {
+    const resultSchedule: CalendarEvent[] = groupSchedule.map((item) => {
       const calendarId = (): "online" | "offline" | "learn" => {
         if (item.teacher.startsWith("https://")) {
           return "learn";
@@ -76,9 +76,12 @@ export function useSchedule() {
       };
       const newEvent = {
         id: item.id,
-        title: item.courseName,
-        description: `Teacher: ${item.teacher}\n${item.location === "online" ? "Online" : "Location: " + item.location}\nType: ${item.type}\n`,
-        rrule: "FREQ=WEEKLY;COUNT=1",
+        title:
+          item.courseName.length > 30
+            ? item.courseName.slice(0, 26) + "..."
+            : item.courseName,
+        description: `${item.teacher.startsWith("https://learn") ? "learn.astanait.edu.kz" : item.teacher}  |  ${item.location.toUpperCase()}  |  ${item.type}\n`,
+        // rrule: "FREQ=WEEKLY;COUNT=1",
       };
 
       const startBaseDate = getTargetDateByDay(item.start);
@@ -87,18 +90,21 @@ export function useSchedule() {
         new Date(new Date(start).setMinutes(startBaseDate.getMinutes() + 50)),
       );
 
-      return [
-        {
-          ...newEvent,
-          start: start,
-          end: end,
-          calendarId: calendarId(),
+      return {
+        ...newEvent,
+        start: start,
+        end: end,
+        _customContent: {
+          timeGrid: `<strong>
+                      ${item.courseName.length > 30 ? item.courseName.slice(0, 26) + "..." : item.courseName}
+                     </strong><br>
+                     ${item.teacher}<br />
+                     ${item.location.toUpperCase()}<br />`,
         },
-      ];
+        calendarId: calendarId(),
+      };
     });
 
-    console.log(resultSchedule);
-    console.log(group);
     return resultSchedule;
   });
 
