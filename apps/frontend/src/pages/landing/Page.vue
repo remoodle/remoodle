9670b5049fd42570f90b431f2e741c513340378c
Rnbsov <rnbsov@gmail.com>
2025-01-16 08:41:41 +0500

    🎉 feat: Create landing page route
    Changelog: feature

---
+++ apps/frontend/src/pages/landing/Page.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineOptions({
+  name: "LandingPage",
+});
+</script>
+
+<template>
+  <div className="flex flex-col items-center justify-center h-screen">
+    <header>
+      <nav>
+        <h1>sex</h1>
+      </nav>
+    </header>
+  </div>
+</template>
