c687c2aeb71065f0f46e298876f62a9ad6500694
Alex Sehl <widesehl@gmail.com>
2025-01-19 17:20:15 +0500

    feat: add usr query login

--- apps/frontend/src/app/App.vue
+++ apps/frontend/src/app/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { watch } from "vue";
+import { onMounted, watch } from "vue";
+import type { IUser } from "@remoodle/types";
 import { RouterView, useRoute, useRouter } from "vue-router";
+import { useUrlSearchParams } from "@vueuse/core";
 import { ConfigProvider } from "radix-vue";
 import { useUserStore } from "@/shared/stores/user";
 import { useAppStore } from "@/shared/stores/app";
@@ -21,6 +23,28 @@ const route = useRoute();
 const router = useRouter();
 
 const userStore = useUserStore();
+
+onMounted(async () => {
+  const params = useUrlSearchParams("history");
+
+  const usr = params.usr as string | undefined;
+
+  if (usr) {
+    const data = atob(usr);
+
+    const resp = JSON.parse(data) as {
+      user: IUser;
+      accessToken: string;
+      refreshToken: string;
+    };
+
+    if (!resp.user) {
+      return;
+    }
+
+    userStore.login(resp.accessToken, resp.refreshToken, resp.user);
+  }
+});
 
 watch(
   () => userStore.authorized,
