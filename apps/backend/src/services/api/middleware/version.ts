c93409645dccaedb6ab9ce4df800ce91d0b71528
Alex Sehl <widesehl@gmail.com>
2025-01-22 09:49:43 +0500

    fix: expose Version header

--- apps/backend/src/services/api/middleware/version.ts
+++ apps/backend/src/services/api/middleware/version.ts
@@ -3,6 +3,7 @@ import type { MiddlewareHandler } from "hono";
 export const versionHandler: MiddlewareHandler = (ctx, next) => {
   // eslint-disable-next-line
   ctx.header("Version", process.env.VERSION_TAG || "");
+  ctx.header("Access-Control-Expose-Headers", "Version");
 
   return next();
 };
