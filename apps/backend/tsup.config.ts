e85cbc4fa3eab2b3c4e7ff7b43a9ffa7a5a457a5
Alex Sehl <widesehl@gmail.com>
2025-02-08 23:01:37 +0500

    chore(backend): omit bundle step

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig(() => ({
-  entry: ["src/"],
+  entry: ["src/**/*.ts", "!src/**/*.spec.ts"],
+  bundle: false,
   clean: true,
   format: ["cjs"],
   target: "node20",
