bf93de028c23cfce959034e7857d642c1fbda225
Alex Sehl <widesehl@gmail.com>
2025-02-08 23:30:09 +0500

    chore: dont bundle json

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig(() => ({
-  entry: ["src/**/*.ts", "!src/**/*.spec.ts"],
+  entry: ["src/", "!src/**/*.spec.ts"],
   clean: true,
   format: ["cjs"],
   target: "node20",
+  loader: {
+    ".json": "copy",
+  },
   noExternal: ["@remoodle/utils", "@remoodle/db"],
 }));
