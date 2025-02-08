50d56653b578f2ff555d2f437cfbf6add23840d1
Alex Sehl <widesehl@gmail.com>
2025-02-08 23:07:30 +0500

    chore: kek

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "tsup";
 
 export default defineConfig(() => ({
   entry: ["src/**/*.ts", "!src/**/*.spec.ts"],
-  entry: ["src/"],
   clean: true,
   format: ["cjs"],
   target: "node20",
